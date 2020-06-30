afffb1e55291eb7dba11775f6d4c52ecf641fbd7
Matthew Krieg <mdkrieg@mtu.edu>
2020-06-30 16:47:24 -0500

    added features

--- alpaca.js
+++ alpaca.js
@@ -1,10 +1,10 @@
-const API_KEY = process.env.ALPACA_API_KEY;
-const API_SECRET = process.env.ALPACA_API_SECRET;
+const API_KEY = process.env.APCA_API_KEY_ID || '';
+const API_SECRET = process.env.APCA_API_SECRET_KEY || '';
 const PAPER = true;
 
 module.exports = function(RED) {
-    Alpaca = require('@alpacahq/alpaca-trade-api');
-    alpaca = new Alpaca({
+    var Alpaca = require('@alpacahq/alpaca-trade-api');
+    var alpaca = new Alpaca({
       keyId: API_KEY, 
       secretKey: API_SECRET, 
       paper: PAPER
@@ -13,28 +13,67 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = alpaca.createOrder({
-                symbol: msg.payload.stock, 
-                qty: msg.payload.quantity, 
-                side: msg.payload.side, 
-                type: 'limit', 
-                time_in_force: 'day', 
-                limit_price: msg.payload.price
-            });
-            node.send(msg);
+            const symbol = msg.symbol || config.symbol;
+            const qty = msg.qty || config.qty;
+            const price = msg.price || config.price;
+            const tif = msg.tif || config.tif || "day";
+            const side = msg.side || config.side;
+            alpaca.createOrder({
+                symbol: symbol,
+                qty: qty,
+                side: side,
+                type: 'limit',
+                time_in_force: tif,
+                limit_price: price
+            }).then(function(resp) {
+                msg.payload = resp;
+                node.send(msg);
+            }).catch((err) => {console.log(err.error);});
         });
     }
     function getOrder(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = alpaca.getOrders({
+            alpaca.getOrders({
 		      status:'all', 
 		      direction:'asc'
-		    });
-		    node.send(msg);
+		    }).then(function(resp) {
+                msg.payload = resp;
+		        node.send(msg);
+            }).catch((err) => {console.log(err.error);});
 		 });
-	 }
+	}
+    function getAccount(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            alpaca.getAccount().then(function(resp) {
+                msg.payload = resp;
+		        node.send(msg);
+            }).catch((err) => {console.log(err.error);});
+		 });
+	}
+	function getBars(config) {
+	    RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            const symbol = msg.symbol || config.symbol;
+            const limit = msg.limit || config.limit || 1;
+            alpaca.getBars(
+                'minute',
+                symbol,
+                {limit: limit}
+                ).then(function(resp) {
+                    msg.payload = resp;
+		            node.send(msg);
+                }
+            ).catch((err) => {console.log(err.error);});
+        });
+	}
+	
     RED.nodes.registerType("submit-order",submitOrder);
-    //RED.nodes.registerType("get-order",getOrder);
+    RED.nodes.registerType("get-order",getOrder);
+    RED.nodes.registerType("get-bars",getBars);
+    RED.nodes.registerType("get-account",getAccount);
 }
